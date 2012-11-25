81b876436f72c14fcb5fea25598a47e7143afc9f
Julien Marchand <contact@julien-marchand.fr>
2012-11-26 00:53:46 +0100

    Verrouillage du feature control MSR quand ce n'est pas déjà fait par le BIOS, pour éviter d'avoir à modifier/recompiler BOCHS.

--- src/vmm/arch/msr.c
+++ src/vmm/arch/msr.c
@@ -10,7 +10,7 @@ void msr_read(uint32_t address, uint32_t *eax, uint32_t *edx) {
 }
 
 void msr_write(uint32_t address, uint32_t eax, uint32_t edx) {
-  __asm__ __volatile__("rdmsr" : : "a" (eax), "d" (edx), "c" (address));
+  __asm__ __volatile__("wrmsr" : : "a" (eax), "d" (edx), "c" (address));
 }
 
 void msr_check_feature_control_msr_lock(void) {
@@ -21,16 +21,21 @@ void msr_check_feature_control_msr_lock(void) {
    * Test if vmxon is allowed outside smx mode. Set it if needed, if the msr
    * is not locked by the bios.
    */
-  if ((eax & 0x1) == 0 || (eax & 0x4) == 0) {
+  /*if ((eax & 0x1) == 0 || (eax & 0x4) == 0) {
     eax = eax | 0x5;
     msr_write(MSR_ADDRESS_IA32_FEATURE_CONTROL_MSR, eax, edx);
-  }
-  /*if ((eax & 0x4) == 0) {
+  }*/
+  if ((eax & 0x4) == 0) {
     INFO("need to change FEATURE_CONTROL_MSR to allow vmxon outside smx mode\n");
-    if ((eax & 0x1) == 0) {
+    if ((eax & 0x1) == 1) {
       ERROR("feature control msr locked, change bios configuration\n");
     }
     eax = eax | 0x4;
     msr_write(MSR_ADDRESS_IA32_FEATURE_CONTROL_MSR, eax, edx);
-  }*/
+  }
+  if ((eax & 0x1) == 0) {
+    INFO("feature control msr unlocked, trying to lock\n");
+    eax = eax | 0x1;
+    msr_write(MSR_ADDRESS_IA32_FEATURE_CONTROL_MSR, eax, edx);
+  }
 }
