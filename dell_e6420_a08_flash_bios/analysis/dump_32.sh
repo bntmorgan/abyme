71d040ef7c1ede8f7f4ce8704a7fcd71fa133f8c
Eric Alata <ealata@svc.(none)>
2013-03-01 10:26:07 +0100

    flood

--- dell_e6420_a08_flash_bios/analysis/dump_32.sh
+++ dell_e6420_a08_flash_bios/analysis/dump_32.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-objdump -D -b binary -mi386 -Maddr32,data32 $1
+objdump -D -b binary --adjust-vma=0xffc00000 --start-address=0xfffffea5 -mi386 -Maddr32,data32 $1
--- dell_e6420_a08_flash_bios/analysis/main.py
+++ dell_e6420_a08_flash_bios/analysis/main.py
@@ -6,13 +6,15 @@ import shlex
 def process_text(context, section):
   print(section["text"])
 
-def process_section(context, section):
-  #print("0x%016x:0x%016x	%s" % (section["start"], section["end"], section["type"]))
+def process_image(context, section):
   local_context = {}
   local_context.update(context)
   local_context["count"] = section["end"] - section["start"] + 1
   local_context["skip"] = section["start"] - context["file-offset"]
-  local_context["offset"] = section["start"]
+  local_context["file-offset"] = context["file-offset"]
+  local_context["start"] = section["start"]
+  local_context["dest"] = section["dest"]
+  local_context["end"] = section["end"]
 
   process_stdin = subprocess.PIPE
   commands = []
@@ -22,8 +24,29 @@ def process_section(context, section):
     command = shlex.split(command)
     process = subprocess.Popen(command, stdin=process_stdin, stdout=subprocess.PIPE)
     process_stdin = process.stdout
+  #print(commands)
   result = process.stdout.readlines()
-  #print("> %s" % "\n  | ".join(commands))
+  print(".. image:: %s.png" % (section["dest"]))
+
+def process_section(context, section):
+  local_context = {}
+  local_context.update(context)
+  local_context["count"] = section["end"] - section["start"] + 1
+  local_context["skip"] = section["start"] - context["file-offset"]
+  local_context["file-offset"] = context["file-offset"]
+  local_context["start"] = section["start"]
+  local_context["end"] = section["end"]
+
+  process_stdin = subprocess.PIPE
+  commands = []
+  for command in section_type_command[section["type"]]:
+    command = command % local_context
+    commands.append(command)
+    command = shlex.split(command)
+    process = subprocess.Popen(command, stdin=process_stdin, stdout=subprocess.PIPE)
+    process_stdin = process.stdout
+    process.wait()
+  result = process.stdout.readlines()
 
   print("::")
   print()
@@ -32,20 +55,21 @@ def process_section(context, section):
   print()
 
 section_type_command = {
-  "CODE32": ["udcli -32 -att -v intel -s %(skip)d -c %(count)d -o 0x%(offset)x %(file)s"],
-  "CODE16": ["udcli -16 -att -v intel -s %(skip)d -c %(count)d -o 0x%(offset)x %(file)s"],
+  "CODE32": ["objdump -D -b binary --adjust-vma=0x%(file-offset)x --start-address=0x%(start)x --stop-address=0x%(end)x -mi386 -Maddr32,data32 %(file)s"],
+  "CODE16": ["objdump -D -b binary --adjust-vma=0x%(file-offset)x --start-address=0x%(start)x --stop-address=0x%(end)x -mi386 -Maddr16,data16 %(file)s"],
+  "GRAPH32": ["python3.2 ./disasm-to-graph.py -o 0x%(file-offset)x -s 0x%(start)x -e %(end)x -b 32 -f %(file)s", "dot -Tpng -o %(dest)s.png"],
   "GDT32":  ["hexdump -v -e '2/4 \"%%08x \" \"\\n\"' -s %(skip)s -n %(count)d %(file)s",
-             "python cat.py -s 0x%(offset)x -p 0x08"],
+             "python cat.py -s 0x%(start)x -p 0x08"],
   "DATA32": ["hexdump -v -e '6/4 \"%%08x \" \"\\n\"' -s %(skip)s -n %(count)d %(file)s",
-             "python cat.py -s 0x%(offset)x -p 0x18"],
+             "python cat.py -s 0x%(start)x -p 0x18"],
   "DATA16": ["hexdump -v -e '12/2 \"%%04x \" \"\\n\"' -s %(skip)s -n %(count)d %(file)s",
-             "python cat.py -s 0x%(offset)x -p 0x18"],
+             "python cat.py -s 0x%(start)x -p 0x18"],
   "DATA08": ["hexdump -v -e '24/1 \"%%02x \" \"\\n\"' -s %(skip)s -n %(count)d %(file)s",
-             "python cat.py -s 0x%(offset)x -p 0x18"],
+             "python cat.py -s 0x%(start)x -p 0x18"],
   "TEXT":   ["hexdump -v -e '6/4 \"%%08x \" \"\"' -e '\" \" 24/1 \"%%_p\" \"\\n\"' -s %(skip)s -n %(count)d %(file)s",
-             "python cat.py -s 0x%(offset)x -p 4"],
+             "python cat.py -s 0x%(start)x -p 4"],
   "GDTR32": ["hexdump -v -e '1/2 \"%%04x-\" 1/4 \"%%08x\" \"\\n\"' -s %(skip)s -n 6 %(file)s",
-             "python cat.py -s 0x%(offset)x -p 0x6"]
+             "python cat.py -s 0x%(start)x -p 0x6"]
 }
 
 context_file = open("context", "r")
@@ -67,6 +91,15 @@ for line in sections_file:
     section["start"] = int(section["start"], 16)
     section["end"] = int(section["end"], 16)
     section["handler"] = process_section
+    sections.append(section)
+  elif line.startswith(":image	"):
+    section = line[len(":image	"):]
+    section = section.strip().split("\t")
+    section = section[0].split(":") + section[1:]
+    section = dict(zip(['start', 'end', 'type', 'dest'], section))
+    section["start"] = int(section["start"], 16)
+    section["end"] = int(section["end"], 16)
+    section["handler"] = process_image
     sections.append(section)
   else:
     text = {}
