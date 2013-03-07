b26381ec52168edf01784f4ac240137d7d6bb40e
garage <garage@noob.(none)>
2013-03-07 11:06:10 +0100

    Ajout d'un outil de désassemblage pour les binaires 32bit avec offset start et offset end

---
+++ tools/dump_32.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+start=0
+end=0
+file=
+
+if [ "$#" -lt 1 ];then
+  echo "$0 file [ start-address stop-address ]"
+  exit 1
+fi
+
+file=$1
+
+if [ "$#" -gt 1 ];then
+  start=$2
+fi
+
+if [ "$#" -lt 3 ];then
+  end=`cat $1 | wc -c`
+else
+  end=$3
+fi
+
+echo $file $start $end
+
+objdump -D -b binary $file -mi386 --start-addres $start --stop-address $end
