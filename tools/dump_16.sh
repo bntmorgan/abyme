a0b9ed29027c7214ee0feb672b5fb106ede16ce2
garage <garage@noob.(none)>
2013-03-07 11:07:46 +0100

    Amélioration de dump16.sh ajout d'adresse de début et addresse de fin

--- tools/dump_16.sh
+++ tools/dump_16.sh
@@ -1,2 +1,25 @@
 #!/bin/sh
-objdump -D -b binary -mi386 -Maddr16,data16 $1
+start=0
+end=0
+file=
+
+if [ "$#" -lt 1 ];then
+  echo "$0 file [ start-address stop-address ]"
+  exit 1
+fi
+
+file=$1
+
+if [ "$#" -gt 1 ];then
+  start=$2
+fi
+
+if [ "$#" -lt 3 ];then
+  end=`cat $1 | wc -c`
+else
+  end=$3
+fi
+
+echo $file $start $end
+
+objdump -D -b binary -mi386 -Maddr16,data16 $file --start-address $start --stop-address $end
