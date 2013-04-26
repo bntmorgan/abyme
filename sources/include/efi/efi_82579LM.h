4a5b05fe037131f7e583c0bf83743029d52da6ea
garage <garage@noob.(none)>
2013-04-26 17:27:03 +0200

    Dump socket raw python

--- sources/include/efi/efi_82579LM.h
+++ sources/include/efi/efi_82579LM.h
@@ -8,6 +8,15 @@
 #define EFI_PROTOCOL_82579LM_GUID {0xcc2ac9d1, 0x14a9, 0x11d3,\
     {0x8e, 0x77, 0x0, 0xa0, 0xc9, 0x69, 0x72, 0x3b}}; 
 
+typedef struct _debug_message_vmexit_notification {
+  uint32_t type;
+  uint32_t exit_reason;
+} debug_message_vmexit_notification;
+
+typedef union _debug_message {
+  uint32_t type;
+} debug_message;
+
 typedef struct _protocol_82579LM {
   uint32_t (*send)(const void *, uint32_t, uint8_t);
   uint32_t (*revc)(void *, uint32_t, uint8_t);
