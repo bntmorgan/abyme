fdf06da9a42f994d154b5f521340a852d9f888a4
garage <garage@noob.(none)>
2013-05-16 17:31:58 +0200

    Dump core state

--- sources/uefi_drivers/uefi_vmm/debug_server/debug_server.c
+++ sources/uefi_drivers/uefi_vmm/debug_server/debug_server.c
@@ -3,6 +3,9 @@
 
 #include "stdio.h"
 #include "string.h"
+#include "vmm.h"
+#include "vmcs.h"
+#include "cpu.h"
 #include "debug_server.h"
   
 protocol_82579LM *eth;
@@ -47,12 +50,48 @@ void debug_server_handle_memory_write(message_memory_write *mr) {
   debug_server_send(&r, sizeof(r));
 }
 
-void debug_server_run(uint32_t exit_reason) {
+void debug_server_get_segment_regs(core_regs *regs) {
+  regs->cs = cpu_vmread(GUEST_CS_SELECTOR);
+  regs->ds = cpu_vmread(GUEST_DS_SELECTOR);
+  regs->ss = cpu_vmread(GUEST_SS_SELECTOR);
+  regs->es = cpu_vmread(GUEST_ES_SELECTOR);
+  regs->fs = cpu_vmread(GUEST_FS_SELECTOR);
+  regs->gs = cpu_vmread(GUEST_GS_SELECTOR);
+}
+
+void debug_server_create_regs(message_vmexit *m, struct registers *regs) {
+  // Copy segment regs
+  debug_server_get_segment_regs(&m->regs); 
+  // Copy GPRs rsp, rbp, rsi, rdi and rip
+  m->regs.rax = regs->rax;
+  m->regs.rbx = regs->rbx;
+  m->regs.rcx = regs->rcx;
+  m->regs.rdx = regs->rdx;
+  m->regs.r8 = regs->r8;
+  m->regs.r9 = regs->r9;
+  m->regs.r10 = regs->r10;
+  m->regs.r11 = regs->r11;
+  m->regs.r12 = regs->r12;
+  m->regs.r13 = regs->r13;
+  m->regs.r14 = regs->r14;
+  m->regs.r15 = regs->r15;
+  // Copy index registers
+  m->regs.rsi = regs->rsi;
+  m->regs.rdi = regs->rdi;
+  // Copy pointer registers
+  m->regs.rbp = regs->rbp;
+  m->regs.rsp = regs->rsp;
+  // Copy instruction pointer register
+  m->regs.rip = regs->rip;
+}
+
+void debug_server_run(uint32_t exit_reason, struct registers *regs) {
   message_vmexit ms = {
     MESSAGE_VMEXIT,
     debug_server_get_core(),
     exit_reason
   };
+  debug_server_create_regs(&ms, regs);
   debug_server_send(&ms, sizeof(ms));
   uint8_t buf[eth->mtu];
   message *mr = (message *)buf;
