beda3602267ce101f11e0795db56ba4316ea619c
Benoît Morgan <bmorgan@laas.fr>
2013-10-04 14:08:49 +0200

    Literale et deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,37 +4,46 @@ PYTHON=python3
 MODULES=modules
 DESTINATION=sources
 
-# Tous les dossiers traités en tant que dossier de code sont de niveau 2 dans
-# l'arborescence
-
 map_folder() {
   local DIR=$1
   local FUNCTION=$2
-  local FOLDERS=`ls $MODULES/$DIR`
+  local FOLDERS=`ls $MODULES$DIR`
   local f
-  echo $FOLDERS
+  echo -e "  "$FOLDERS
+  echo
+  local TAB="$3."
   for f in $FOLDERS; do
-    $FUNCTION $DIR/$f
+    $FUNCTION $DIR/$f $TAB
   done
 }
 
 do_deploy() {
-  echo Deploiment de : $MODULES$1
   if [ -f $MODULES$1 ]; then
-    echo Fichier à copier
-    echo cp -fpr $MODULES$1 $DESTINATION$1
-    cp -fpr $MODULES$1 $DESTINATION$1
+    echo $2 Deploiement de : $MODULES$1
+    if [ "${1#*.}" = "w" ]; then
+      filename=`basename $1`
+      if [ "${filename%.*}"  = "root" ]; then
+        echo $2 Fichier web racine
+        python3.3 literale/prepare.py -b `dirname $MODULES$1` `basename $1` | python3.3 literale/tangle.py -d `dirname $DESTINATION$1`
+      fi
+    else
+      cp -fpr $MODULES$1 $DESTINATION$1
+    fi
   fi
   if [ -d $MODULES$1 ]; then
+    echo $2 Deploiement de : $MODULES$1/
     mkdir -p $DESTINATION$1
-    map_folder $1 do_deploy
+    map_folder $1 do_deploy $2
   fi
 }
 
 do_all() {
   echo Dossier de travail : $MODULES
-  map_folder "" do_deploy
+  echo Dossier destination : $DESTINATION
+  echo
+  echo Deploiement de : $MODULES
+  map_folder "" do_deploy ""
 }
 
-# Entry point
+# Point d'entrée
 do_all
