9836fdfb910bc4a3204b877f7f49d2d00bea3009
Benoît Morgan <bmorgan@laas.fr>
2013-11-14 16:40:34 +0100

    cpuid tool and efi bin

---
+++ modules/uefi_applications/cpuid/efi.c
@@ -0,0 +1,20 @@
+#include <efi.h>
+#include <efilib.h>
+
+EFI_STATUS efi_main(EFI_HANDLE image, EFI_SYSTEM_TABLE *systab) {
+  InitializeLib(image, systab);
+
+	uint64_t v;
+	uint64_t b;
+	uint64_t c;
+	uint64_t d;
+	v = 0x99999999;
+	Print(L"v=%lx\n", v);
+	__asm__ __volatile__("cpuid" : "=a"(v), "=b"(b), "=c"(c), "=d"(d) : "a"(v));
+	Print(L"v=%lx\n", v);
+	Print(L"b=%lx\n", b);
+	Print(L"c=%lx\n", c);
+	Print(L"d=%lx\n", d);
+
+  return EFI_SUCCESS;
+}
