c101ce5947b8c5741430896fae4dfc67f794df93
Guillaume Averlant <gaverlan@laas.fr>
2014-07-04 17:54:09 +0200

    Ajout des directives de lecture et écriture des registres PCI

--- modules/uefi_drivers/uefi_vmm/pci.c
+++ modules/uefi_drivers/uefi_vmm/pci.c
@@ -3,11 +3,48 @@
 
 #include "types.h"
 #include "pci.h"
+#include "cpu.h"
 #include "efi/efi_82579LM.h"
 
 uint8_t protect;
 
 extern protocol_82579LM *eth;
+
+uint8_t pci_readb(uint32_t id, uint32_t reg) {
+  uint32_t addr = 0x80000000 | id | (reg & 0xfc);
+  cpu_outportd(PCI_CONFIG_ADDR, addr);
+  return cpu_inportb(PCI_CONFIG_DATA + (reg & 0x03));
+}
+
+uint16_t pci_readw(uint32_t id, uint32_t reg) {
+  uint32_t addr = 0x80000000 | id | (reg & 0xfc);
+  cpu_outportd(PCI_CONFIG_ADDR, addr);
+  return cpu_inportw(PCI_CONFIG_DATA + (reg & 0x02));
+}
+
+uint32_t pci_readd(uint32_t id, uint32_t reg) {
+  uint32_t addr = 0x80000000 | id | (reg & 0xfc);
+  cpu_outportd(PCI_CONFIG_ADDR, addr);
+  return cpu_inportd(PCI_CONFIG_DATA);
+}
+
+void pci_writeb(uint32_t id, uint32_t reg, uint8_t data) {
+    uint32_t addr = 0x80000000 | id | (reg & 0xfc);
+    cpu_outportd(PCI_CONFIG_ADDR, addr);
+    cpu_outportb(PCI_CONFIG_DATA + (reg & 0x03), data);
+}
+
+void pci_writew(uint32_t id, uint32_t reg, uint16_t data) {
+    uint32_t addr = 0x80000000 | id | (reg & 0xfc);
+    cpu_outportd(PCI_CONFIG_ADDR, addr);
+    cpu_outportw(PCI_CONFIG_DATA + (reg & 0x02), data);
+}
+
+void pci_writed(uint32_t id, uint32_t reg, uint32_t data) {
+    uint32_t addr = 0x80000000 | id | (reg & 0xfc);
+    cpu_outportd(PCI_CONFIG_ADDR, addr);
+    cpu_outportd(PCI_CONFIG_DATA, data);
+}
 
 // XXX On considère value comme étant toujours sur 32 bits
 uint8_t pci_no_protect_out(uint16_t port, uint32_t value) {
--- modules/uefi_drivers/uefi_vmm/pci.h
+++ modules/uefi_drivers/uefi_vmm/pci.h
@@ -3,7 +3,8 @@
 
 #include <efi.h>
 
-#define PCI_MAKE_ID(bus, dev, func)     ((bus) << 16) | ((dev) << 11) | ((func) << 8)
+#define PCI_MAKE_ID(bus, dev, func)     (((bus) << 16) | ((dev) << 11) | ((func) << 8))
+#define PCI_MAKE_MMCONFIG(bus, dev, func)     ((((bus) << 8) | ((dev) << 3) | (func)) << 12) 
 
 // 
 // I/O Ports
@@ -14,5 +15,12 @@
 inline uint32_t pci_make_addr(uint32_t id);
 uint8_t pci_no_protect_out(uint16_t port, uint32_t value);
 uint8_t pci_no_protect_in(uint16_t port);
+
+uint8_t pci_readb(uint32_t id, uint32_t reg);
+uint16_t pci_readw(uint32_t id, uint32_t reg);
+uint32_t pci_readd(uint32_t id, uint32_t reg);
+void pci_writeb(uint32_t id, uint32_t reg, uint8_t data);
+void pci_writew(uint32_t id, uint32_t reg, uint16_t data);
+void pci_writed(uint32_t id, uint32_t reg, uint32_t data);
 
 #endif
