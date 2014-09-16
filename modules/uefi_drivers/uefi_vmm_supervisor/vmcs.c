dfbe3628e73c110a58a62c92fb3e269b919d3f31
Guillaume Averlant <gaverlan@laas.fr>
2014-09-16 18:23:59 +0200

    Une modification de CR3 ne déclanche plus de vm_exit Nous cachons désormais CR4.VMXE au guest

--- modules/uefi_drivers/uefi_vmm_supervisor/vmcs.c
+++ modules/uefi_drivers/uefi_vmm_supervisor/vmcs.c
@@ -171,7 +171,7 @@ void vmcs_fill_vm_exec_control_fields(void) {
   procbased_ctls = cpu_adjust32(procbased_ctls, MSR_ADDRESS_IA32_VMX_PROCBASED_CTLS);
   if (((msr_read(MSR_ADDRESS_IA32_VMX_BASIC) >> 55) & 1)                       // extra capabilities enabled
   && (((msr_read(MSR_ADDRESS_IA32_VMX_TRUE_PROCBASED_CTLS) >> 15) & 3)) == 0){ // CR3_LOAD_EXITING & CR3_STORE_EXITING can be disabled
-    procbased_ctls &= ~(/*CR3_LOAD_EXITING |*/ CR3_STORE_EXITING);
+    procbased_ctls &= ~(CR3_LOAD_EXITING | CR3_STORE_EXITING);
   } else {
     panic("#!PROCBASED_CTLS CR3_LOAD_EXITING or CR3_STORE_EXITING couldn't be disabled\n");
   }
@@ -197,7 +197,7 @@ void vmcs_fill_vm_exec_control_fields(void) {
   cpu_vmwrite(CR0_GUEST_HOST_MASK, (msr_read(MSR_ADDRESS_VMX_CR0_FIXED0) | (~msr_read(MSR_ADDRESS_VMX_CR0_FIXED1))) & ~(0x80000001));
   cpu_vmwrite(CR0_READ_SHADOW, cpu_read_cr0());
   cpu_vmwrite(CR4_GUEST_HOST_MASK, msr_read(MSR_ADDRESS_VMX_CR4_FIXED0) | ~msr_read(MSR_ADDRESS_VMX_CR4_FIXED1));
-  cpu_vmwrite(CR4_READ_SHADOW, cpu_read_cr4());
+  cpu_vmwrite(CR4_READ_SHADOW, cpu_read_cr4() & ~(uint64_t)(1<<13)); // We hide CR4.VMXE
 
   cpu_vmwrite(CR3_TARGET_COUNT, 0);
   cpu_vmwrite(CR3_TARGET_VALUE0, 0);
