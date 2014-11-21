ca6151f62c2c35f9290c91e948ee3c9ac599fa61
Benoît Morgan <bmorgan@laas.fr>
2014-11-21 21:17:47 +0100

    Driver eric !

---
+++ modules/uefi_drivers/eric/efi.c
@@ -0,0 +1,143 @@
+#include <efi.h>
+#include <efilib.h>
+
+#include "pci.h"
+#include "efi/efi_eric.h"
+#include "debug.h"
+#include "stdio.h"
+
+#define ERIC_VENDOR_ID  0x1AA5
+#define ERIC_DEVICE_ID  0xE51C
+
+// 
+// stdio putc pointer
+//
+extern void (*putc)(uint8_t);
+
+void put_nothing(uint8_t c) {}
+
+void eric_disable_debug(void){
+  putc = &put_nothing;
+}
+
+//
+// Protocol structure definition
+//
+protocol_eric proto = {
+  .pci_addr = {
+    0,
+    0,
+    0
+  },
+  0,
+  0,
+  0,
+  0
+};
+
+EFI_STATUS rt_unload (IN EFI_HANDLE image);
+EFI_GUID guid_eric = EFI_PROTOCOL_ERIC_GUID;
+
+EFI_STATUS efi_main(EFI_HANDLE image_handle, EFI_SYSTEM_TABLE *systab) {
+  InitializeLib(image_handle, systab);
+
+  EFI_STATUS status;
+  EFI_LOADED_IMAGE *image;
+  uint32_t id;
+  uint8_t *bar0, *bar1, *bar2, *rom;
+  pci_device_addr addr;
+  pci_device_info info;
+  pci_device_info_ext info_ext;
+
+  INFO("Experimental Intel 82579LM Ethernet driver initialization\n");
+  if (pci_get_device(ERIC_VENDOR_ID, ERIC_DEVICE_ID, &addr)) {
+    INFO("FAILED to locate ERIC\n");
+    return EFI_NOT_FOUND;
+  }
+
+  id = PCI_MAKE_ID(addr.bus, addr.device, addr.function);
+
+  // Powerup PIO & MMI_extO
+  pci_writew(id, PCI_CONFIG_COMMAND, 0x7);
+  pci_writeb(id, PCI_CONFIG_INTERRUPT_LINE, 0x5);
+  
+  INFO("COMMAND 0x%04x\n", pci_readw(id, PCI_CONFIG_COMMAND));
+
+  pci_get_device_info(&info, id);
+  pci_get_device_info_ext(&info_ext, id);
+  pci_print_device_info(&info);
+  pci_print_device_info_ext(&info_ext);
+
+  // Read bar0
+
+  pci_bar bar;
+  pci_get_bar(&bar, id, 0);
+  if (bar.flags & PCI_BAR_IO) {
+    // Only Memory Mapped I/O supported
+    INFO("Only Memory Mapped I/O supported\n");
+    return -1;
+  }
+  bar0 = (uint8_t *)bar.u.address;
+
+  pci_get_bar(&bar, id, 1);
+  if (bar.flags & PCI_BAR_IO) {
+    // Only Memory Mapped I/O supported
+    INFO("Only Memory Mapped I/O supported\n");
+    return -1;
+  }
+  bar1 = (uint8_t *)bar.u.address;
+
+  pci_get_bar(&bar, id, 2);
+  if (bar.flags & PCI_BAR_IO) {
+    // Only Memory Mapped I/O supported
+    INFO("Only Memory Mapped I/O supported\n");
+    return -1;
+  }
+  bar2 = (uint8_t *)bar.u.address;
+
+  rom = (uint8_t *)(uintptr_t)pci_readd(id,
+      PCI_CONFIG_EXPANSION_ROM_BASE_ADDRESS);
+
+  pci_writed(id, PCI_CONFIG_EXPANSION_ROM_BASE_ADDRESS,
+      ((uint32_t)(uintptr_t)rom) | 1);
+
+  INFO("BAR0 address 0x%08x\n", bar0);
+  dump(bar0, 2, 0x10, (uint32_t)(uintptr_t)bar0, 2); 
+  INFO("BAR1 address 0x%08x\n", bar1);
+  dump(bar1, 2, 0x10, (uint32_t)(uintptr_t)bar1, 2); 
+  INFO("BAR2 address 0x%08x\n", bar2);
+  dump(bar2, 2, 0x10, (uint32_t)(uintptr_t)bar2, 2); 
+  INFO("ROM address 0x%08x\n", rom);
+  dump(rom, 2, 0x10, (uint32_t)(uintptr_t)rom, 2); 
+
+  // Add an unload handler
+  status = uefi_call_wrapper(BS->HandleProtocol, 4, &image_handle,
+      &LoadedImageProtocol, (void*)&image);
+  ASSERT (!EFI_ERROR(status));
+  image->Unload = rt_unload;
+
+  INFO("Unload handler added %x\n", (uintptr_t)image_handle);
+
+  // Share the pci address in the uefi protocol
+  pci_device_addr *pci_addr = &addr;
+  proto.pci_addr.bus = pci_addr->bus;
+  proto.pci_addr.device = pci_addr->device;
+  proto.pci_addr.function = pci_addr->function;
+  proto.bar0 = (uint32_t *)bar0;
+  proto.bar1 = (uint32_t *)bar1;
+  proto.bar2 = (uint32_t *)bar2;
+  proto.rom = (uint32_t *)rom;
+
+  // Add a protocol so someone can locate us
+  status = uefi_call_wrapper(BS->InstallProtocolInterface, 4, &image_handle,
+      &guid_eric, (EFI_INTERFACE_TYPE)NULL, &proto);
+  ASSERT (!EFI_ERROR(status));
+
+  // eric_disable_debug();
+
+  return status;
+}
+
+EFI_STATUS rt_unload (IN EFI_HANDLE image) {
+  return (EFI_SUCCESS);
+}
---
+++ modules/uefi_drivers/eric/pci.c
@@ -0,0 +1,232 @@
+#include <efi.h>
+#include <efilib.h>
+
+#include "types.h"
+#include "pci.h"
+#include "cpu.h"
+#include "stdio.h"
+
+uint8_t pci_readb(uint32_t id, uint32_t reg) {
+  uint32_t addr = 0x80000000 | id | (reg & 0xfc);
+  cpu_outportd(PCI_CONFIG_ADDR, addr);
+  return cpu_inportb(PCI_CONFIG_DATA + (reg & 0x03));
+}
+
+uint16_t pci_readw(uint32_t id, uint32_t reg) {
+  uint32_t addr = 0x80000000 | id | (reg & 0xfc);
+  cpu_outportd(PCI_CONFIG_ADDR, addr);
+  return cpu_inportw(PCI_CONFIG_DATA + (reg & 0x02));
+}
+
+uint32_t pci_readd(uint32_t id, uint32_t reg) {
+  uint32_t addr = 0x80000000 | id | (reg & 0xfc);
+  cpu_outportd(PCI_CONFIG_ADDR, addr);
+  return cpu_inportd(PCI_CONFIG_DATA);
+}
+
+void pci_writeb(uint32_t id, uint32_t reg, uint8_t data) {
+    uint32_t addr = 0x80000000 | id | (reg & 0xfc);
+    cpu_outportd(PCI_CONFIG_ADDR, addr);
+    cpu_outportb(PCI_CONFIG_DATA + (reg & 0x03), data);
+}
+
+void pci_writew(uint32_t id, uint32_t reg, uint16_t data) {
+    uint32_t addr = 0x80000000 | id | (reg & 0xfc);
+    cpu_outportd(PCI_CONFIG_ADDR, addr);
+    cpu_outportw(PCI_CONFIG_DATA + (reg & 0x02), data);
+}
+
+void pci_writed(uint32_t id, uint32_t reg, uint32_t data) {
+    uint32_t addr = 0x80000000 | id | (reg & 0xfc);
+    cpu_outportd(PCI_CONFIG_ADDR, addr);
+    cpu_outportd(PCI_CONFIG_DATA, data);
+}
+
+void pci_get_device_info(pci_device_info *info, uint32_t id) {
+  info->vendor_id = pci_readw(id, PCI_CONFIG_VENDOR_ID);
+  info->device_id = pci_readw(id, PCI_CONFIG_DEVICE_ID);
+  info->command = pci_readw(id, PCI_CONFIG_COMMAND);
+  info->status = pci_readw(id, PCI_CONFIG_STATUS);
+
+  info->revision_id = pci_readb(id, PCI_CONFIG_REVISION_ID);
+  info->prog_intf = pci_readb(id, PCI_CONFIG_PROG_INTF);
+  info->sub_class = pci_readb(id, PCI_CONFIG_SUBCLASS);
+  info->class_code = pci_readb(id, PCI_CONFIG_CLASS_CODE);
+  info->cacheline_size = pci_readb(id, PCI_CONFIG_CACHELINE_SIZE);
+  info->latency_timer = pci_readb(id, PCI_CONFIG_LATENCY_TIMER);
+  info->header_type = pci_readb(id, PCI_CONFIG_HEADER_TYPE);
+  info->bist = pci_readb(id, PCI_CONFIG_BIST);
+
+  info->bar0 = pci_readd(id, PCI_CONFIG_BAR0);
+  info->bar1 = pci_readd(id, PCI_CONFIG_BAR1);
+  info->bar2 = pci_readd(id, PCI_CONFIG_BAR2);
+  info->bar3 = pci_readd(id, PCI_CONFIG_BAR3);
+  info->bar4 = pci_readd(id, PCI_CONFIG_BAR4);
+  info->bar5 = pci_readd(id, PCI_CONFIG_BAR5);
+  info->cardbus_cis_pointer = pci_readd(id, PCI_CONFIG_CARDBUS_CIS_POINTER);
+
+  info->subsystem_vendor_id = pci_readw(id, PCI_CONFIG_SUBSYSTEM_VENDOR_ID);
+  info->subsystem_id = pci_readw(id, PCI_CONFIG_SUBSYSTEM_ID);
+
+  info->expansion_rom_base_address = pci_readd(id, PCI_CONFIG_EXPANSION_ROM_BASE_ADDRESS);
+
+  info->capability_pointer = pci_readb(id, PCI_CONFIG_CAPABILITY_POINTER);
+  info->interrupt_line = pci_readb(id, PCI_CONFIG_INTERRUPT_LINE);
+  info->interrupt_pin = pci_readb(id, PCI_CONFIG_INTERRUPT_PIN);
+  info->min_gnt = pci_readb(id, PCI_CONFIG_MIN_GNT);
+  info->max_latency = pci_readb(id, PCI_CONFIG_MAX_LATENCY);
+}
+
+void pci_get_device_info_ext(pci_device_info_ext *info, uint32_t id) {
+  info->clist1 = pci_readw(id, PCI_CONFIG_CLIST1);
+  info->pmc = pci_readw(id, PCI_CONFIG_PMC);
+  info->pmcs = pci_readw(id, PCI_CONFIG_PMCS);
+
+  info->dr = pci_readb(id, PCI_CONFIG_DR);
+
+  info->clist2 = pci_readw(id, PCI_CONFIG_CLIST2);
+  info->mctl = pci_readw(id, PCI_CONFIG_MCTL);
+
+  info->maddl = pci_readd(id, PCI_CONFIG_MADDL);
+  info->maddh = pci_readd(id, PCI_CONFIG_MADDH);
+
+  info->mdat = pci_readw(id, PCI_CONFIG_MDAT);
+  info->flrcap = pci_readw(id, PCI_CONFIG_FLRCAP);
+  info->flrclv = pci_readw(id, PCI_CONFIG_FLRCLV);
+  info->devctrl = pci_readw(id, PCI_CONFIG_DEVCTRL);
+}
+
+void pci_print_device_info(pci_device_info *info) {
+  Print(L"info->vendor_id %04x\n", info->vendor_id);
+  Print(L"info->device_id %04x\n", info->device_id);
+  Print(L"info->command %04x\n", info->command);
+  Print(L"info->status %04x\n", info->status);
+  
+  Print(L"info->revision_id %02x\n", info->revision_id);
+  Print(L"info->prog_intf %02x\n", info->prog_intf);
+  Print(L"info->sub_class %02x\n", info->sub_class);
+  Print(L"info->class_code %02x\n", info->class_code);
+  Print(L"info->cacheline_size %02x\n", info->cacheline_size);
+  Print(L"info->latency_timer %02x\n", info->latency_timer);
+  Print(L"info->header_type %02x\n", info->header_type);
+  Print(L"info->bist %02x\n", info->bist);
+
+  Print(L"info->bar0 %08x\n", info->bar0);
+  Print(L"info->bar1 %08x\n", info->bar1);
+  Print(L"info->bar2 %08x\n", info->bar2);
+  Print(L"info->bar3 %08x\n", info->bar3);
+  Print(L"info->bar4 %08x\n", info->bar4);
+  Print(L"info->bar5 %08x\n", info->bar5);
+
+  Print(L"info->cardbus_cis_pointer %08x\n", info->cardbus_cis_pointer);
+
+  Print(L"info->subsystem_vendor_id %04x\n", info->subsystem_vendor_id);
+  Print(L"info->subsystem_id %04x\n", info->subsystem_id);
+
+  Print(L"info->expansion_rom_base_address %08x\n", info->expansion_rom_base_address);
+
+  Print(L"info->capability_pointer %02x\n", info->capability_pointer);
+  Print(L"info->interrupt_line %02x\n", info->interrupt_line);
+  Print(L"info->interrupt_pin %02x\n", info->interrupt_pin);
+  Print(L"info->min_gnt %02x\n", info->min_gnt);
+  Print(L"info->max_latency %02x\n", info->max_latency);
+}
+
+void pci_print_device_info_ext(pci_device_info_ext *info) {
+  Print(L"info->clist1 %04x\n", info->clist1); // pci_readw
+  Print(L"info->pmc %04x\n", info->pmc); // pci_readw
+  Print(L"info->pmcs %04x\n", info->pmcs); // pci_readw
+
+  Print(L"info->dr %02x\n", info->dr); // pci_readb
+
+  Print(L"info->clist2 %04x\n", info->clist2); // pci_readw
+  Print(L"info->mctl %04x\n", info->mctl); // pci_readw
+
+  Print(L"info->maddl %08x\n", info->maddl); // pci_readd
+  Print(L"info->maddh %08x\n", info->maddh); // pci_readd
+
+  Print(L"info->mdat %04x\n", info->mdat); // pci_readw
+  Print(L"info->flrcap %04x\n", info->flrcap); // pci_readw
+  Print(L"info->flrclv %04x\n", info->flrclv); // pci_readw
+  Print(L"info->devctrl %04x\n", info->devctrl); // pci_readw
+}
+
+int pci_get_device(uint32_t vendor_id, uint32_t device_id, pci_device_addr *addr) {
+  uint8_t ok = 0;
+  uint16_t bus;
+  uint8_t dev;
+  for (bus = 0; bus < 256; bus++) {
+    for (dev = 0; dev < 32 && ok == 0; dev++) {
+      uint32_t base_id = PCI_MAKE_ID(bus, dev, 0);
+      uint8_t header_type = pci_readb(base_id, PCI_CONFIG_HEADER_TYPE);
+      uint32_t func_count = header_type & PCI_TYPE_MULTIFUNC ? 8 : 1;
+      uint32_t func;
+      for (func = 0; func < func_count; func++) {
+        uint32_t id = PCI_MAKE_ID(bus, dev, func);
+        uint32_t vid = pci_readw(id, PCI_CONFIG_VENDOR_ID);
+        uint32_t did = pci_readw(id, PCI_CONFIG_DEVICE_ID);
+        if (vid != 0xffff) {
+          INFO("[%04x:%04x] %02x:%02x:%02x\n", vid, did, bus, dev, func);
+        }
+        if (vid == 0xffff || vid != vendor_id || did != device_id) {
+          continue;
+        } else {
+          addr->bus = bus;
+          addr->device = dev;
+          addr->function = func;
+          ok = 1;
+          break;
+        }
+      }
+    }
+  }
+  if (ok) {
+    return 0;
+  } else {
+    return -1;
+  }
+}
+
+void pci_read_bar(uint32_t id, uint32_t index, uint32_t *address, uint32_t *mask) {
+  uint32_t reg = PCI_CONFIG_BAR0 + index * sizeof(uint32_t);
+  // Get address
+  *address = pci_readd(id, reg);
+  // Find out size of the bar
+  pci_writed(id, reg, 0xffffffff);
+  *mask = pci_readd(id, reg);
+  // Restore adddress
+  pci_writed(id, reg, *address);
+}
+
+void pci_get_bar(pci_bar *bar, uint32_t id, uint32_t index) {
+  // Read pci bar register
+  uint32_t addressLow;
+  uint32_t maskLow;
+  pci_read_bar(id, index, &addressLow, &maskLow);
+
+  if (addressLow & PCI_BAR_64)
+  {
+    // 64-bit mmio
+    uint32_t addressHigh;
+    uint32_t maskHigh;
+    pci_read_bar(id, index + 1, &addressHigh, &maskHigh);
+
+    bar->u.address = (void *)(((uintptr_t)addressHigh << 32) | (addressLow & ~0xf));
+    bar->size = ~(((uint64_t)maskHigh << 32) | (maskLow & ~0xf)) + 1;
+    bar->flags = addressLow & 0xf;
+  }
+  else if (addressLow & PCI_BAR_IO)
+  {
+    // i/o register
+    bar->u.port = (uint16_t)(addressLow & ~0x3);
+    bar->size = (uint16_t)(~(maskLow & ~0x3) + 1);
+    bar->flags = addressLow & 0x3;
+  }
+  else
+  {
+    // 32-bit mmio
+    bar->u.address = (void *)(uintptr_t)(addressLow & ~0xf);
+    bar->size = ~(maskLow & ~0xf) + 1;
+    bar->flags = addressLow & 0xf;
+  }
+}
