99578e4edcfec0e6d693ed802833f950cc2f7f02
Benoît Morgan <bmorgan@laas.fr>
2016-04-18 16:48:34 +0159

    New hello world with info

--- sources/applications/hello_world/efi.c
+++ sources/applications/hello_world/efi.c
@@ -1,10 +1,11 @@
 #include <efi.h>
 #include <efilib.h>
+#include "stdio.h"
 
 EFI_STATUS efi_main(EFI_HANDLE image, EFI_SYSTEM_TABLE *systab) {
   InitializeLib(image, systab);
 
-  uefi_call_wrapper(systab->ConOut->OutputString, 2, systab->ConOut, L"Hello World\n\r");
+  INFO("Hello World\n");
 
   return EFI_SUCCESS;
 }
