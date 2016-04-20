4a5edde2b0c10524d035e8839226b1957346f5a5
Benoît Morgan <bmorgan@laas.fr>
2016-04-20 11:34:56 +0159

    Ça compile pas ... désactivation

--- sources/applications/rules.mk
+++ sources/applications/rules.mk
@@ -24,7 +24,9 @@ dir	:= $(d)/shadowck
 include	$(dir)/rules.mk
 dir	:= $(d)/mp
 include	$(dir)/rules.mk
-dir := $(d)/udp_stack
+# dir := $(d)/udp_stack
+# include $(dir)/rules.mk
+dir := $(d)/ping
 include $(dir)/rules.mk
 
 d		:= $(dirstack_$(sp))
