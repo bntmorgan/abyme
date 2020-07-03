1df18c98b4761bc31f7c148ff72c43babccd9fba
MORGAN Benoit <benoit.morgan@enseeiht.fr>
2020-07-03 17:34:07 +0200

    New eth_send

--- sources/applications/eth_send/efi.c
+++ sources/applications/eth_send/efi.c
@@ -6,19 +6,14 @@
 #include "string.h"
 #include "stdio.h"
 
-#define BUF2_SIZE 1000
-
 EFI_STATUS efi_main(EFI_HANDLE image, EFI_SYSTEM_TABLE *systab) {
   InitializeLib(image, systab);
   EFI_STATUS status;
   protocol_82579LM *eth;
-  uint32_t i;
-  uint16_t len = 0x100;
-  uint8_t buf[0x100];
-  uint8_t buf2[BUF2_SIZE];
-  struct ethernet_header *frame = (struct ethernet_header *)&buf2[0];
-  uint8_t *data = &buf2[0] + sizeof(struct ethernet_header);
+  char buf[ETHERNET_SIZE];
   EFI_GUID guid_82579LM = EFI_PROTOCOL_82579LM_GUID;
+  uint32_t len;
+  union ethernet_buffer *eb = (union ethernet_buffer *)&buf[0];
 
   status = LibLocateProtocol(&guid_82579LM, (void **)&eth);
   if (EFI_ERROR(status)) {
@@ -26,42 +21,24 @@ EFI_STATUS efi_main(EFI_HANDLE image, EFI_SYSTEM_TABLE *systab) {
     return -1;
   }
 
-  for (i = 0; i < len ; i++) {
-    buf[i] = 0xca;
-  }
-
-  // TODO Pourquoi uefi_call_wrapper ne marche pas ??
-  //uefi_call_wrapper(eth->send, 3, buf, len, 0);
-  INFO("Sending with old ehternet API !\n");
-  eth->send(buf, len, 0);
-
-  // We copy source mac address
-  memcpy(&frame->srcmac[0], &eth->mac_addr[0], 6);
-  // Dst address
-  INFO("Dest mac address\n");
-  for (i = 0; i < 6; i++) {
-    frame->destmac[i] = 0xff;
-    printk("%02x ", frame->destmac[i]);
-  }
-  printk("\n");
-
-  INFO("Source mac address\n");
-  for (i = 0; i < 6; i++) {
-    printk("%02x ", frame->srcmac[i]);
-  }
-  printk("\n");
-
-  frame->ethertype = htons(0xb00b);
-
-  // We put some data
-  for (i = 0; i < BUF2_SIZE - sizeof(struct ethernet_header); i++) {
-    data[i] = 0xca;
-  }
-
-  INFO("Sending with new ehternet API !\n");
-  eth->eth_send(buf2, BUF2_SIZE, 1);
-
   INFO("Pointeur du service eth %x\n", eth);
+
+  // Initialize microUDP
+  microudp_start(eth->mac_addr, SERVER_IP);
+
+  // Start with ARP request
+  INFO("ARP who-has 192.168.0.1 sent\n");
+  memset(&buf[0], 0, ETHERNET_SIZE);
+  len = microudp_start_arp(eb, CLIENT_IP, ARP_OPCODE_REQUEST);
+  dump(buf, 2, len, 8, (uint32_t)(uintptr_t)buf, 2, 0);
+  eth->eth_send(buf, len, 1);
+
+  INFO("Wait for ARP is-at reply\n");
+  memset(&buf[0], 0, ETHERNET_SIZE);
+  eth->eth_recv(buf, ETHERNET_SIZE, 1);
+  microudp_handle_frame(eb);
+
+  INFO("DONE\n");
 
   return EFI_SUCCESS;
 }
